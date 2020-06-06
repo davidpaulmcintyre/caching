9b2d243946d8bcfeea1755120531c320c107c0d2
David McIntyre <davidpmcintyre@gmail.com>
2020-06-05 23:01:17 -0400

    mysql

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var Mysql      = require('mysql');
 var mysql = Mysql.createConnection({
     host     : process.env.MYSQL_URL,
     port      :  process.env.MYSQL_PORT,
-    user     : process.env.MYSQL_PORT,
+    user     : process.env.MYSQL_USERNAME,
     password : process.env.MYSQL_PASSWORD,
     database : process.env.MYSQL_DB
 });
